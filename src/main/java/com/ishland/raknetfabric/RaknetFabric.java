5b954fb08c459952b989329db1311e2c890d62f6
ishland <ishlandmc@yeah.net>
2022-01-20 20:08:45 +0800

    Initial commit

---
+++ src/main/java/com/ishland/raknetfabric/RaknetFabric.java
@@ -0,0 +1,10 @@
+package com.ishland.raknetfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RaknetFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
