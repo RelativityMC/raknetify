0d224795e9fd5c75ed208f74daf69336cf5c6337
ishland <ishlandmc@yeah.net>
2022-01-24 15:35:10 +0800

    Increase max queued size to 256MB

--- src/main/java/com/ishland/raknetfabric/Constants.java
+++ src/main/java/com/ishland/raknetfabric/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String RAKNET_PREFIX = "raknet;";
     public static final int RAKNET_PACKET_ID = 0xFD;
-    public static final int MAX_QUEUED_SIZE = 32 * 1024 * 1024;
+    public static final int MAX_QUEUED_SIZE = 256 * 1024 * 1024;
     public static final int DEFAULT_MTU = 1400;
 
     private Constants() {
