7387fa7dfd27c668b6ea5ce00d737af73e28ef2c
ishland <ishlandmc@yeah.net>
2022-01-24 15:49:11 +0800

    Print stacktrace when errored

--- src/main/java/com/ishland/raknetfabric/mixin/common/MixinClientConnection.java
+++ src/main/java/com/ishland/raknetfabric/mixin/common/MixinClientConnection.java
@@ -5,6 +5,7 @@ import io.netty.bootstrap.AbstractBootstrap;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.epoll.Epoll;
 import io.netty.channel.epoll.EpollDatagramChannel;
 import io.netty.channel.socket.SocketChannel;
@@ -21,7 +22,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.net.InetSocketAddress;
 
@@ -67,6 +70,11 @@ public abstract class MixinClientConnection {
         return ThreadLocalUtil.isInitializingRaknet()
                 ? instance.channelFactory(() -> new RakNetClientChannel(actuallyUseEpoll ? EpollDatagramChannel.class : NioDatagramChannel.class))
                 : instance.channel(aClass);
+    }
+
+    @Inject(method = "exceptionCaught", at = @At("HEAD"))
+    private void onExceptionCaught(ChannelHandlerContext context, Throwable ex, CallbackInfo ci) {
+        ex.printStackTrace();
     }
 
 }
