fedeb724fc188cb6cca03c027281b5f6620b3057
ishland <ishlandmc@yeah.net>
2022-02-07 18:29:35 +0800

    Yet another throughput improvement

--- src/main/java/com/ishland/raknetfabric/common/connection/RaknetConnectionUtil.java
+++ src/main/java/com/ishland/raknetfabric/common/connection/RaknetConnectionUtil.java
@@ -4,9 +4,11 @@ import com.ishland.raknetfabric.Constants;
 import com.ishland.raknetfabric.common.compat.viafabric.ViaFabricCompatInjector;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelInitializer;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import net.minecraft.server.ServerNetworkIo;
 import network.ycc.raknet.RakNet;
+import network.ycc.raknet.server.channel.RakNetApplicationChannel;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,25 +25,42 @@ public class RaknetConnectionUtil {
             config.setRetryDelayNanos(TimeUnit.NANOSECONDS.convert(25, TimeUnit.MILLISECONDS));
             config.setDefaultPendingFrameSets(Constants.DEFAULT_PENDING_FRAME_SETS);
             config.setNACKEnabled(false);
-            final SimpleMetricsLogger simpleMetricsLogger = new SimpleMetricsLogger();
-            config.setMetrics(simpleMetricsLogger);
-            final MetricsSynchronizationHandler metricsSynchronizationHandler = new MetricsSynchronizationHandler();
-            simpleMetricsLogger.setMetricsSynchronizationHandler(metricsSynchronizationHandler);
+
+            initRaknetChannel(channel);
+
 //            channel.pipeline().addLast("raknetfabric-flush-enforcer", new FlushEnforcer());
-            channel.pipeline().addLast("raknetfabric-metrics-sync", metricsSynchronizationHandler);
 //            channel.pipeline().addLast("raknetfabric-flush-consolidation", new FlushConsolidationHandler(Integer.MAX_VALUE, true));
             channel.pipeline().addLast("raknetfabric-no-flush", new NoFlush());
-            channel.pipeline().addLast("raknetfabric-synchronization-layer", new SynchronizationLayer(1));
             channel.pipeline().addLast("raknetfabric-multi-channel-data-codec", new MultiChannellingDataCodec(Constants.RAKNET_GAME_PACKET_ID));
-            channel.pipeline().addLast("raknetfabric-threading", new ChannelDuplexHandler());
         }
+    }
+
+    private static void initRaknetChannel(Channel appChannel) {
+        final Channel channel;
+        if (appChannel instanceof RakNetApplicationChannel) {
+            channel = appChannel.parent();
+        } else {
+            channel = appChannel;
+        }
+        channel.pipeline().addLast(new ChannelInitializer<>() {
+            @Override
+            protected void initChannel(Channel ch) {
+                final RakNet.Config config = (RakNet.Config) ch.config();
+                final SimpleMetricsLogger simpleMetricsLogger = new SimpleMetricsLogger();
+                config.setMetrics(simpleMetricsLogger);
+                final MetricsSynchronizationHandler metricsSynchronizationHandler = new MetricsSynchronizationHandler();
+                simpleMetricsLogger.setMetricsSynchronizationHandler(metricsSynchronizationHandler);
+                ch.pipeline().addLast("raknetfabric-metrics-sync", metricsSynchronizationHandler);
+                ch.pipeline().addLast("raknetfabric-synchronization-layer", new SynchronizationLayer(1));
+                ch.pipeline().addFirst("raknetfabric-timeout", new ReadTimeoutHandler(15));
+            }
+        });
     }
 
     public static void postInitChannel(Channel channel, boolean isClientSide) {
         if (channel.config() instanceof RakNet.Config) {
             ViaFabricCompatInjector.inject(channel, isClientSide);
             channel.pipeline().replace("timeout", "timeout", new ChannelDuplexHandler()); // no-op
-            channel.pipeline().addFirst("raknetfabric-timeout", new ReadTimeoutHandler(15));
             channel.pipeline().replace("splitter", "splitter", new ChannelDuplexHandler()); // no-op
             channel.pipeline().replace("prepender", "prepender", new ChannelDuplexHandler()); // no-op
             final MultiChannellingPacketCapture handler = new MultiChannellingPacketCapture();
