1e46f5c218fbfbf85ab666ec0299b5854b1913d8
ishland <ishlandmc@yeah.net>
2022-02-26 17:36:18 +0800

    Workaround for #1, #3

--- src/main/java/com/ishland/raknetfabric/mixin/compat/fabricapi/MixinServerLoginNetworkAddon.java
+++ src/main/java/com/ishland/raknetfabric/mixin/compat/fabricapi/MixinServerLoginNetworkAddon.java
@@ -2,6 +2,7 @@ package com.ishland.raknetfabric.mixin.compat.fabricapi;
 
 import com.ishland.raknetfabric.mixin.access.IClientConnection;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,6 +22,10 @@ public class MixinServerLoginNetworkAddon {
     @Final
     private ClientConnection connection;
 
+    @Shadow
+    @Final
+    private MinecraftServer server;
+
     @SuppressWarnings("DefaultAnnotationParam")
     @Dynamic("Pseudo")
     @Inject(method = "sendCompressionPacket()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", ordinal = 0, shift = At.Shift.AFTER, remap = true), remap = false)
@@ -28,12 +33,12 @@ public class MixinServerLoginNetworkAddon {
         ((IClientConnection) this.connection).getChannel().eventLoop().execute(() -> {
             try {
                 try {
-                    this.connection.setCompressionThreshold(512, false);
+                    this.connection.setCompressionThreshold(this.server.getNetworkCompressionThreshold(), false);
                 } catch (NoSuchMethodError e) {
-                    System.out.println("An error occurred when starting compression, using alternative method: ");
+                    System.out.println("An error occurred when starting compression, using alternative method: " + e.toString());
                     //noinspection JavaReflectionMemberAccess
                     final Method method_10760 = ClientConnection.class.getMethod("method_10760", int.class);
-                    method_10760.invoke(this.connection, 512);
+                    method_10760.invoke(this.connection, this.server.getNetworkCompressionThreshold());
                 }
             } catch (Throwable t) {
                 t.printStackTrace();
--- src/main/java/com/ishland/raknetfabric/mixin/server/MixinServerLoginNetworkHandler.java
+++ src/main/java/com/ishland/raknetfabric/mixin/server/MixinServerLoginNetworkHandler.java
@@ -2,6 +2,7 @@ package com.ishland.raknetfabric.mixin.server;
 
 import com.ishland.raknetfabric.mixin.access.IClientConnection;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,17 +18,19 @@ public class MixinServerLoginNetworkHandler {
 
     @Shadow @Final public ClientConnection connection;
 
+    @Shadow @Final private MinecraftServer server;
+
     @Inject(method = "acceptPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V", ordinal = 0, shift = At.Shift.AFTER))
     private void setupDummyCompressionImmediately(CallbackInfo ci) throws Throwable {
         ((IClientConnection) this.connection).getChannel().eventLoop().execute(() -> {
             try {
                 try {
-                    this.connection.setCompressionThreshold(512, false);
+                    this.connection.setCompressionThreshold(this.server.getNetworkCompressionThreshold(), false);
                 } catch (NoSuchMethodError e) {
-                    System.out.println("An error occurred when starting compression, using alternative method: ");
+                    System.out.println("An error occurred when starting compression, using alternative method: " + e.toString());
                     //noinspection JavaReflectionMemberAccess
                     final Method method_10760 = ClientConnection.class.getMethod("method_10760", int.class);
-                    method_10760.invoke(this.connection, 512);
+                    method_10760.invoke(this.connection, this.server.getNetworkCompressionThreshold());
                 }
             } catch (Throwable t) {
                 t.printStackTrace();
