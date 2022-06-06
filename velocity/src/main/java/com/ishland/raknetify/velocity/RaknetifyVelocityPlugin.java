3fa2b79dff8109862544fd3a82579cd5dd1d6cb9
ishland <ishlandmc@yeah.net>
2022-06-06 17:55:12 +0800

    Fix respawn in velocity

--- velocity/src/main/java/com/ishland/raknetify/velocity/RaknetifyVelocityPlugin.java
+++ velocity/src/main/java/com/ishland/raknetify/velocity/RaknetifyVelocityPlugin.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.ishland.raknetify.common.connection.RakNetSimpleMultiChannelCodec;
 import com.ishland.raknetify.common.data.ProtocolMultiChannelMappings;
 import com.ishland.raknetify.velocity.connection.RakNetVelocityConnectionUtil;
+import com.ishland.raknetify.velocity.init.VelocityPacketRegistryInjector;
 import com.ishland.raknetify.velocity.init.VelocityRaknetifyServer;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
@@ -34,6 +35,7 @@ public class RaknetifyVelocityPlugin {
         LOGGER = this.logger;
 
         ProtocolMultiChannelMappings.init();
+        VelocityPacketRegistryInjector.inject();
 
         PROXY.getEventManager().register(this, LoginEvent.class, PostOrder.LAST, RakNetVelocityConnectionUtil::onPlayerLogin);
         PROXY.getEventManager().register(this, ListenerBoundEvent.class, PostOrder.LAST, VelocityRaknetifyServer::start);
---
+++ velocity/src/main/java/com/ishland/raknetify/velocity/init/VelocityPacketRegistryInjector.java
@@ -0,0 +1,40 @@
+package com.ishland.raknetify.velocity.init;
+
+import com.ishland.raknetify.common.util.ReflectionUtil;
+import com.velocitypowered.api.network.ProtocolVersion;
+import com.velocitypowered.proxy.protocol.MinecraftPacket;
+import com.velocitypowered.proxy.protocol.StateRegistry;
+import com.velocitypowered.proxy.protocol.packet.Respawn;
+import io.netty.util.collection.IntObjectMap;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static com.ishland.raknetify.common.util.ReflectionUtil.accessible;
+
+public class VelocityPacketRegistryInjector {
+
+    public static void inject() {
+        try {
+            final StateRegistry.PacketRegistry registry = StateRegistry.PLAY.clientbound;
+            final var versions = (Map<ProtocolVersion, StateRegistry.PacketRegistry.ProtocolRegistry>)
+                    accessible(StateRegistry.PacketRegistry.class.getDeclaredField("versions")).get(registry);
+            for (StateRegistry.PacketRegistry.ProtocolRegistry value : versions.values()) {
+                final var packetClassToId = (Object2IntMap<Class<? extends MinecraftPacket>>)
+                        accessible(StateRegistry.PacketRegistry.ProtocolRegistry.class.getDeclaredField("packetClassToId")).get(value);
+                final var packetIdToSupplier = (IntObjectMap<Supplier<? extends MinecraftPacket>>)
+                        accessible(StateRegistry.PacketRegistry.ProtocolRegistry.class.getDeclaredField("packetIdToSupplier")).get(value);
+                if (packetClassToId.containsKey(Respawn.class)) {
+                    final int packetId = packetClassToId.getInt(Respawn.class);
+                    if (!packetIdToSupplier.containsKey(packetId)) {
+                        packetIdToSupplier.put(packetId, Respawn::new); // make respawn packet no longer encodeOnly
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException("Failed to inject velocity packet registry", t);
+        }
+    }
+
+}
