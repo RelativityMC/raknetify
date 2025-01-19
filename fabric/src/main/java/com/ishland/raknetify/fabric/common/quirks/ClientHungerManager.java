0fee59e5885c754bdbc0e30c851ccddac5840e28
ishland <ishlandmc@yeah.net>
2025-01-19 17:18:56 +0800

    fix: hunger level desync
    A client-side fix, always trust server for hunger levels
    
    Fixes #33

---
+++ fabric/src/main/java/com/ishland/raknetify/fabric/common/quirks/ClientHungerManager.java
@@ -0,0 +1,49 @@
+/*
+ * This file is a part of the Raknetify project, licensed under MIT.
+ *
+ * Copyright (c) 2022-2023 ishland
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.ishland.raknetify.fabric.common.quirks;
+
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.player.HungerManager;
+import net.minecraft.nbt.NbtCompound;
+
+public class ClientHungerManager extends HungerManager {
+
+    public static ClientHungerManager from(HungerManager hungerManager) {
+        ClientHungerManager clientHungerManager = new ClientHungerManager();
+        NbtCompound compound = new NbtCompound();
+        hungerManager.writeNbt(compound);
+        clientHungerManager.readNbt(compound);
+        return clientHungerManager;
+    }
+
+    public void add(int food, float saturationModifier) {
+        // nop
+    }
+
+    public void eat(FoodComponent foodComponent) {
+        // nop
+    }
+
+}
